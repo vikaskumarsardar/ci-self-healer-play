4033e002f42d564b1b042e4df6e7a3398eb2c642
vikaskumarsardar <swapankumarsardar73727@gmail.com>
2026-09-04 20:49:11 +0530

    bump: version 1.2.33 (fix model parameter overrides)

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@
  *   author: swapankumar <swapankumarsardar73727@gmail.com>
  *   url: https://github.com/vikaskumarsardar/ci-self-healer-play
  * metadata:
- *   version: 1.2.32
+ *   version: 1.2.33
  *   status: released
  *   kind: atomic
  *   flow_type: sequential
